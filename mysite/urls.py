73a3f0f34208ee503177b09958e9ccff982c5691
yonggyulee <yg9677@gmail.com>
2020-09-04 17:26:50 +0900

    static file 처리

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import main.views as mainviews
 
 urlpatterns = [
+    path('', mainviews.index),
     path('admin/', admin.site.urls),
 ]
