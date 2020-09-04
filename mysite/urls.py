92e4d6af85f4d764318fce65878ae06c9863cfd7
yonggyulee <yg9677@gmail.com>
2020-09-04 17:44:43 +0900

    guestbook application #1

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import main.views as mainviews
+import guestbook.views as guestbookviews
 
 urlpatterns = [
     path('', mainviews.index),
+    path('guestbook/', guestbookviews.index),
     path('admin/', admin.site.urls),
 ]
